17935edcce894bc149456685213da0c64d35c1a4
Klupzor <anloges@hotmail.com>
2018-02-20 21:03:45 -0500

    se crea la funcion en gulp Con esto se crea app.js en public basado en index.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var babel = require('babelify')
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+var rename = require('gulp-rename')
+// var presents = require('babel-preset-env')
+var sourcemaps = require('gulp-sourcemaps')
+var buffer = require('vinyl-buffer')
+var watchify = require('watchify')
+
+
+
 
 gulp.task('styles', function () {
     gulp
@@ -14,4 +25,46 @@ gulp.task('assets', function (){
     .pipe(gulp.dest('public'))
 })
 
-gulp.task('default', ['styles', 'assets'])
+// gulp.task('scripts', function(){
+//     browserify('./src/index.js')
+//     .transform(babel, {presents: ["env"]})
+//     .bundle()
+//     .pipe(source('index.js'))
+//     .pipe(rename('app.js'))
+//     .pipe(gulp.dest('public'))
+
+// })
+
+function compile(watch) {
+    var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel));
+  
+    function rebundle() {
+      bundler.bundle()
+        .on('error', function(err) { console.error(err); this.emit('end'); })
+        .pipe(source('index.js'))
+        .pipe(rename('app.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('public'));
+    }
+  
+    if (watch) {
+      bundler.on('update', function() {
+        console.log('-> bundling...');
+        rebundle();
+      });
+    }
+  
+    rebundle();
+  }
+  
+  function watch() {
+    return compile(true);
+  };
+  
+  gulp.task('build', function() { return compile(); });
+  gulp.task('watch', function() { return watch(); });
+
+
+gulp.task('default', ['styles', 'assets', 'watch'])
