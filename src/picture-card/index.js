97dc2cda7eb99a19e724366c1de4ef7f9cd76b57
Klupzor <anloges@hotmail.com>
2018-02-23 14:51:07 -0500

    Agregamos el codigo para que soporte safari

--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,5 +1,10 @@
 var yo = require('yo-yo')
 
+if (!window.Intl){
+  window.Intl = require('intl')
+  require('intl/locale-data/jsonp/en-US.js')
+  require('intl/locale-data/jsonp/es.js')
+}
 var IntlRelativeFormat = window.IntlRelativeFormat = require('intl-relativeformat')
 require('intl-relativeformat/dist/locale-data/en.js')
 require('intl-relativeformat/dist/locale-data/es.js')
