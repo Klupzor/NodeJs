3da30d7cc3d71adc7147fde8cbb80044fd392bb1
Klupzor <anloges@hotmail.com>
2018-02-23 23:42:34 -0500

    Se instala superagent En creatAt sale un error de formato por eso se le agrega el signo '+'. Tembien se separo el header

--- server.js
+++ server.js
@@ -17,6 +17,37 @@ app.get('/signin', function(req, res){
     res.render('index', {title: 'Platzigram-Signin'})
 })
 
+app.get('/api/pictures', function(req, res, next){
+    var pictures = [{
+        user: {
+            username: 'andres',
+            avatar: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2016/12/iron-man-armadura.jpg'
+
+        },
+        url: 'office.jpg',
+        likes: 0,
+        liked: false,
+        createdAt: +new Date()
+    },
+    {
+        user: {
+            username: 'pedro',
+            avatar: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2016/12/iron-man-armadura.jpg'
+
+        },
+        url: 'office.jpg',
+        likes: 1,
+        liked: true,
+        createdAt: +new Date().setDate(new Date().getDate() - 10)
+    }
+
+    ]
+    setTimeout(function(){
+        res.send(pictures)
+    }, 2000)
+
+})
+
 app.listen(3000, function (err){
     if (err) return console.log('Hubo un error'), process.exit(1)
 
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,39 +1,26 @@
 var page = require('page')
 var empty = require('empty-element')
 var template = require('./template')
+var request = require('superagent')
+var header = require('../header')
 
 
 
-
-page('/', function (ctx,next){
+page('/', header, loadPictures, function (ctx,next){
     // title('Platzigram-Signin')
     document.title='Platzigram'
     var main = document.getElementById('main-container')
 
-    var pictures = [{
-        user: {
-            username: 'andres',
-            avatar: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2016/12/iron-man-armadura.jpg'
-
-        },
-        url: 'office.jpg',
-        likes: 0,
-        liked: false,
-        createdAt: new Date()
-    },
-    {
-        user: {
-            username: 'pedro',
-            avatar: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2016/12/iron-man-armadura.jpg'
-
-        },
-        url: 'office.jpg',
-        likes: 1,
-        liked: true,
-        createdAt: new Date().setDate(new Date().getDate() - 10)
-    }
-
-]
-
-    empty(main).appendChild(template(pictures))
+    empty(main).appendChild(template(ctx.pictures))
 })
+
+function loadPictures(ctx, next) {
+    request
+        .get('/api/pictures')
+        .end(function(err, res){
+            if (err) return console.log(err)
+
+            ctx.pictures = res.body
+            next()
+        })
+}
