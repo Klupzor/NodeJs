383a484d603c4e1579aa80c54d9cdf99ab3bbdae
Klupzor <anloges@hotmail.com>
2018-02-26 15:58:54 -0500

    Se crea la carpeta uploads  y se sube el archivo

--- public/app.js
+++ public/app.js
@@ -13319,13 +13319,14 @@ var yo = require('yo-yo');
 var layout = require('../layout');
 var picture = require('../picture-card');
 var translate = require('../translate').message;
+var request = require('superagent');
 
 module.exports = function (pictures) {
     var el = yo`
 <div class="container timeline">
     <div class="row">
         <div class="col s12 m10 offset-m1 l8 offset-12 center-align">
-            <form enctype="multipart/form-data" class="form-upload" id="formUpload">
+            <form enctype="multipart/form-data" class="form-upload" id="formUpload" onsubmit=${onsubmit}>
                 <div id="fileName" class="fileUpload btn btn-flat cyan">
                     <span><i class="fa fa-camera" aria-hidden="true"></i>${translate('upload-picture')}</span>
                     <input name="picture" id="file" type="file" class="upload" onchange=${onchange}/>
@@ -13362,10 +13363,19 @@ module.exports = function (pictures) {
         toogleButtons();
     }
 
+    function onsubmit(ev) {
+        ev.preventDefault();
+
+        var data = new FormData(this);
+        request.post('/api/pictures').send(data).end(function (err, res) {
+            console.log(arguments);
+        });
+    }
+
     return layout(el);
 };
 
-},{"../layout":77,"../picture-card":78,"../translate":85,"yo-yo":69}],75:[function(require,module,exports){
+},{"../layout":77,"../picture-card":78,"../translate":85,"superagent":64,"yo-yo":69}],75:[function(require,module,exports){
 var page = require('page');
 
 require('./homepage');
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -2,6 +2,7 @@ var yo = require('yo-yo')
 var layout = require('../layout')
 var picture = require('../picture-card')
 var translate = require('../translate').message
+var request = require('superagent')
 
 
 module.exports = function (pictures){
@@ -9,7 +10,7 @@ var el =  yo`
 <div class="container timeline">
     <div class="row">
         <div class="col s12 m10 offset-m1 l8 offset-12 center-align">
-            <form enctype="multipart/form-data" class="form-upload" id="formUpload">
+            <form enctype="multipart/form-data" class="form-upload" id="formUpload" onsubmit=${onsubmit}>
                 <div id="fileName" class="fileUpload btn btn-flat cyan">
                     <span><i class="fa fa-camera" aria-hidden="true"></i>${translate('upload-picture')}</span>
                     <input name="picture" id="file" type="file" class="upload" onchange=${onchange}/>
@@ -45,6 +46,20 @@ function cancel(){
 
 function onchange() {
     toogleButtons()
+}
+
+function onsubmit (ev){
+    ev.preventDefault()
+
+    var data = new FormData(this)
+    request 
+        .post('/api/pictures')
+        .send(data)
+        .end(function (err, res){
+            console.log(arguments)
+        })
+
+
 }
 
 return layout(el)
